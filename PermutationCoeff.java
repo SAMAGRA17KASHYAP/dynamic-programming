afe0c0b580b601273fa5798966bb488918d68097
SAMAGRA17KASHYAP <kashyap.samagraa@gmail.com>
2019-07-04 23:13:28 +0530

    cinn

---
+++ PermutationCoeff.java
@@ -0,0 +1,33 @@
+class Main
+{
+	public static void main(String []argc)
+	{
+		int n= Integer.parseInt(argc[0]);
+		int k = Integer.parseInt(argc[1]);
+		System.out.println(impl1(n,k));
+	}
+	public static int impl1(int n,int k)
+	{
+		int[][] data = new int[n+1][k+1];
+		for(int i=0;i<=n;i++)
+		{
+
+			for(int t=0;t<=Math.min(k,i);t++)
+			{
+				if(t==0)
+				{
+					data[i][t]=1;
+				}
+				else
+				{
+					data[i][t] = t * data[i-1][t-1] + data[i-1][t];
+				}
+			}
+		}
+		return data[n][k];
+	}
+
+	public static int withExtraSpace(int n, int k)
+	{
+	}
+}
